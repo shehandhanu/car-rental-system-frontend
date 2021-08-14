8a8ab842809ffba9ea31a83f6555fd0d525c4f29
thara99 <it19148014@my.sliit.lk>
2021-08-15 00:17:50 +0530

    add insert forms ui

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import Header from '../src/components/Header/Header'
 import Home from '../src/components/Home/Home'
 import Footer from '../src/components/Footer/Footer'
+import EmInsertion from '../src/components/Employee/EmployeeInsertion/EmployeeInsertion'
+import ReIsue from '../src/components/Employee/ReportIssues/ReportIssues'
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
-      <Footer />
+      {/* <Header />
+      <Home /> */}
+       {/* <EmInsertion />  */}
+      {/* <Footer /> */}
+      <ReIsue/>
     </div>
   );
 }
