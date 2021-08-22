2a7d2384ca6324035b730ecab68aa6a202a5bfbb
Hashini Mudannayake <hashie.mudannayake@gmail.com>
2021-08-22 11:09:51 +0530

    basic functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import './App.css';
-import Header from '../src/components/Header/Header'
-import Home from '../src/components/Home/Home'
-import Footer from '../src/components/Footer/Footer'
+import "./App.css";
+import Header from "../src/components/Header/Header";
+import Home from "../src/components/Home/Home";
+import Footer from "../src/components/Footer/Footer";
+import VehicleRegistration from "./components/Vehicle/VehicleRegistration";
+import OwnerRegistration from "./components/Vehicle/OwnerRegistration";
+import VehicleInformation from "./components/Vehicle/VehicleInformation";
+import OwnerInformation from "./components/Vehicle/OwnerInformation";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
+      {/* <Home /> */}
+      {/* <VehicleRegistration /> */}
+      {/* <OwnerRegistration /> */}
+      {/* <VehicleInformation /> */}
+      <OwnerInformation />
       <Footer />
     </div>
   );
