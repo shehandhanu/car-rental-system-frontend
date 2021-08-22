d7729d7bcc9d54805ace908616bcd76443df36c4
Shahan Dhanuddara <65756207+shehandhanu@users.noreply.github.com>
2021-08-22 14:05:45 +0530

    tharaka bug fixed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import EmInsertion from '../src/components/Employee/EmployeeInsertion/EmployeeInsertion'
 import ReIsue from '../src/components/Employee/ReportIssues/ReportIssues'
 import AdminHome from '../src/components/Employee/AdminHome/AdminHome'
 import EmployeeHome from '../src/components/Employee/EmployeeHome/EmployeeHome'
-import UserProfile from '../src/components/Employee/UserProfile/UserProfile'
+import EmpUserProfile from '../src/components/Employee/UserProfile/UserProfile'
 import EmployeeUpdate from '../src/components/Employee/EmployeeUpdate/EmployeeUpdate'
 import EmployeeList from './components/Employee/EmployeeList/EmployeeList'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
 import Footer from './components/Footer/Footer'
@@ -27,20 +27,20 @@ function App() {
   return (
     <Router>
       <Header />
-     <Route path="/employeeinsert" component={EmInsertion} exact />  
-     <Route path="/reportvehicleissue" component={ReIsue} exact /> 
-      <Route path="/adminhome" component={AdminHome} exact />
-      <Route path="/employeehome" component={EmployeeHome} exact/>
-      <Route path="/employeeupdate" component={EmployeeUpdate} exact/>
-      <Route path ="/userprofile" component={UserProfile} exact /> 
-      <Route path = "/employeelist" component={EmployeeList} exact  />
       <Route path="/" component={Home} exact />
+      <Route path="/employeeinsert" component={EmInsertion} exact />
+      <Route path="/reportvehicleissue" component={ReIsue} exact />
+      <Route path="/adminhome" component={AdminHome} exact />
+      <Route path="/employeehome" component={EmployeeHome} exact />
+      <Route path="/employeeupdate" component={EmployeeUpdate} exact />
+      <Route path="/userprofile" component={UserProfile} exact />
+      <Route path="/employeelist" component={EmployeeList} exact />
       <Route path="/forgotpassword" component={ForgotPassword} exact />
       <Route path="/forgotpasswordemail" component={ForgotPasswordEmail} exact />
       <Route path="/changepassword" component={ChangePassword} exact />
       <Route path="/signin" component={SignIn} exact />
       <Route path="/signup" component={SignUp} exact />
-      <Route path="/profile" component={UserProfile} exact />
+      <Route path="/profile" component={EmpUserProfile} exact />
       <Route path="/userreport" component={UserReport} exact />
       <Route path="/userhistory" component={TripHistory} exact />
       <Route path="/payment" component={Payment} exact />
