4fa5bc1428d1bb46abfac8f950e7b41ca5b9d4bb
Hashini Mudannayake <hashie.mudannayake@gmail.com>
2021-09-29 02:59:23 +0530

    old changes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import EmInsertion from '../src/components/Employee/EmployeeInsertion/EmployeeInsertion'
-import ReIsue from '../src/components/Employee/ReportIssues/ReportIssues'
-import AdminHome from '../src/components/Employee/AdminHome/AdminHome'
-import EmployeeHome from '../src/components/Employee/EmployeeHome/EmployeeHome'
-import EmpUserProfile from '../src/components/Employee/UserProfile/UserProfile'
-import EmployeeUpdate from '../src/components/Employee/EmployeeUpdate/EmployeeUpdate'
-import EmployeeList from './components/Employee/EmployeeList/EmployeeList'
-import Header from './components/Header/Header'
-import Home from './components/Home/Home'
-import Footer from './components/Footer/Footer'
-import SignIn from './components/Sign In/SignIn'
-import SignUp from './components/Sign Up/SignUp'
-import ForgotPassword from './components/ForgotPassword/ForgotPassword'
-import ForgotPasswordEmail from './components/ForgotPassword/ForgotPasswordEmail'
-import ChangePassword from './components/PasswordChange/PasswordChange'
-import UserProfile from './components/User/UserProfile'
-import UserProfileUpdate from './components/User/UserProfileUpdate'
-import UserReport from './components/User/UserReport'
-import TripHistory from './components/User/TripHistory/TripHistory'
-import Payment from './components/Payments/Payment'
-import CarList from './components/CarSelection/CarList'
-import CarCheckout from './components/CarSelection/CarReservation/CarReservation'
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import EmInsertion from "../src/components/Employee/EmployeeInsertion/EmployeeInsertion";
+import ReIsue from "../src/components/Employee/ReportIssues/ReportIssues";
+import AdminHome from "../src/components/Employee/AdminHome/AdminHome";
+import EmployeeHome from "../src/components/Employee/EmployeeHome/EmployeeHome";
+import EmpUserProfile from "../src/components/Employee/UserProfile/UserProfile";
+import EmployeeUpdate from "../src/components/Employee/EmployeeUpdate/EmployeeUpdate";
+import EmployeeList from "./components/Employee/EmployeeList/EmployeeList";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Footer from "./components/Footer/Footer";
+import SignIn from "./components/Sign In/SignIn";
+import SignUp from "./components/Sign Up/SignUp";
+import ForgotPassword from "./components/ForgotPassword/ForgotPassword";
+import ForgotPasswordEmail from "./components/ForgotPassword/ForgotPasswordEmail";
+import ChangePassword from "./components/PasswordChange/PasswordChange";
+import UserProfile from "./components/User/UserProfile";
+import UserProfileUpdate from "./components/User/UserProfileUpdate";
+import UserReport from "./components/User/UserReport";
+import TripHistory from "./components/User/TripHistory/TripHistory";
+import Payment from "./components/Payments/Payment";
+import CarList from "./components/CarSelection/CarList";
+import CarCheckout from "./components/CarSelection/CarReservation/CarReservation";
+import VehicleRegistration from "./components/Vehicle/vehicleRegistration/VehicleRegistration";
+import VehicleInformation from "./components/Vehicle/vehicleInformation/VehicleInformation";
+import OwnerRegistration from "./components/Vehicle/ownerRegistration/OwnerRegistration";
+import OwnerInformation from "./components/Vehicle/ownerInformation/OwnerInformation";
 
 function App() {
   return (
@@ -36,7 +40,11 @@ function App() {
       <Route path="/userprofile" component={EmpUserProfile} exact />
       <Route path="/employeelist" component={EmployeeList} exact />
       <Route path="/forgotpassword" component={ForgotPassword} exact />
-      <Route path="/forgotpasswordemail" component={ForgotPasswordEmail} exact />
+      <Route
+        path="/forgotpasswordemail"
+        component={ForgotPasswordEmail}
+        exact
+      />
       <Route path="/changepassword" component={ChangePassword} exact />
       <Route path="/signin" component={SignIn} exact />
       <Route path="/signup" component={SignUp} exact />
@@ -47,6 +55,15 @@ function App() {
       <Route path="profileupdate/" component={UserProfileUpdate} exact />
       <Route path="/carlist" component={CarList} exact />
       <Route path="/carcheckout" component={CarCheckout} exact />
+
+      <Route
+        path="/vehicleregistration"
+        component={VehicleRegistration}
+        exact
+      />
+      <Route path="/vehicleinformation" component={VehicleInformation} exact />
+      <Route path="/ownerregistration" component={OwnerRegistration} exact />
+      <Route path="/ownerinformation" component={OwnerInformation} exact />
       <Footer />
     </Router>
   );
